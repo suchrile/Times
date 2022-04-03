b99589e4e462cb160e59c5cfa17389d2645f7255
Artem Ryzhov <suchrile@mail.ru>
2022-04-03 20:30:55 +0300

    Minor changes.

--- src/store/upload.js
+++ src/store/upload.js
@@ -1,22 +1,15 @@
-import { getStorage, ref, uploadBytes, getDownloadURL, deleteObject } from "firebase/storage"
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage"
 import { getAuth, updateProfile } from "firebase/auth"
 
 export default {
   actions: {
     async uploadAvatar({ commit, dispatch, getters }, file) {
       const uid = await dispatch('getUid')
-      if (getters.userData.avatar !== "") {
-        await deleteObject(ref(getStorage(), getters.userData.avatar))
-        updateProfile(getAuth().currentUser, {
-          photoURL: ""
-        })
-      }
-      const avatarRef = ref(getStorage(), `users/${uid}/avatar.${file.name.split('.').pop()}`)
-      uploadBytes(avatarRef, file)
-      updateProfile(getAuth().currentUser, {
+      const avatarRef = ref(getStorage(), `users/${uid}/avatar.jpeg`)
+      await uploadBytes(avatarRef, file)
+      await updateProfile(getAuth().currentUser, {
         photoURL: await getDownloadURL(avatarRef)
       })
-      console.log(getAuth().currentUser)
     }
   }
 }
